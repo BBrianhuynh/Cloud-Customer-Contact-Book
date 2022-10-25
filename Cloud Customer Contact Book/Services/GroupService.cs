16d371a0e92f3e63049afcad418dcc1f6bf48851
BBrianhuynh <92127341+BBrianhuynh@users.noreply.github.com>
2022-10-24 22:15:08 -0700

    Create service layer for group operations

--- Cloud Customer Contact Book/Services/GroupService.cs
+++ Cloud Customer Contact Book/Services/GroupService.cs
@@ -7,7 +7,7 @@ public class GroupService
 {
     private readonly ContactService _contactService;
 
-    public async Task<List<GroupModel>> GetAll()
+    public async Task<List<GroupModel>> GetAll(long? contactId = default)
     {
         var result = GenerateFew(_ => new GroupModel
         {
@@ -18,7 +18,6 @@ public class GroupService
 
         return result.ToList();
     }
-
     public async Task<GroupModel?> Get(long groupId)
     {
         var result = new GroupModel
@@ -30,7 +29,6 @@ public class GroupService
 
         return result;
     }
-
     public async Task<GroupModel> Create([FromBody] GroupCreateModel model)
     {
         var result = new GroupModel
@@ -42,7 +40,6 @@ public class GroupService
 
         return result;
     }
-
     public async Task<GroupModel?> Update(long groupId, [FromBody] GroupCreateModel model)
     {
         var result = new GroupModel
@@ -54,27 +51,35 @@ public class GroupService
 
         return result;
     }
-
     public async Task<bool> Delete(long groupId)
     {
         return true;
     }
-
     public async Task<List<ContactModel>> GetContacts(long groupId)
     {
         var contactIds = GenerateFew(_ => Random.Shared.NextInt64());
 
         return await _contactService.GetByIds(contactIds.ToArray());
     }
-
+    public async Task<bool> AddContactToGroup(long contactId, long groupId)
+    {
+        return true;
+    }
+    public async Task<bool> RemoveContactFromGroup(long contactId, long groupId)
+    {
+        return true;
+    }
+    public async Task<bool> AddContactToGroup_(long contactId, long groupId)
+    {
+        return true;
+    }
+    public async Task<bool> RemoveContactFromGroup_(long contactId, long groupId)
+    {
+        return true;
+    }
     private static IEnumerable<T> GenerateFew<T>(Func<int, T> generator)
     {
         var count = Random.Shared.Next(3, 10);
         return Enumerable.Range(0, count).Select(generator);
-    }
-
-    public GroupService(ContactService contactService)
-    {
-        _contactService = contactService;
     }
 }
